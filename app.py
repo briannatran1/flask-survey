7063a8227566e086dc54584c69b57aa9fc051215
Brianna Tran <briannatran45@gmail.com>
2023-08-24 11:32:31 -0700

    started making question page

--- app.py
+++ app.py
@@ -7,3 +7,21 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.get('/')
+def show_survey_start():
+    """Shows survey title, instructions, and start button"""
+
+    return render_template('survey_start.html',
+                           title=survey.title,
+                           instructions=survey.instructions)
+
+
+@app.post('/begin')
+def show_questions():
+    """Redirects user to first question when clicking button"""
+
+    return redirect('/questions/0')
