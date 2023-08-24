29ad9a07b1b52a884e32690272c1b27cb9d3a979
Daniel Zych <danjzych@gmail.com>
2023-08-24 14:11:35 -0500

    nearing completion of step 5 for survey

--- app.py
+++ app.py
@@ -28,7 +28,34 @@ def show_first_question():
 
 
 @app.get('/questions/<int:num>')
-def show_questions():
+def show_questions(num):
     """Shows form of current question with choices"""
+    question = survey.questions[num]
 
-    return render_template('question.html')
+
+    return render_template('question.html',
+                           prompt=question.prompt,
+                           choices=question.choices)
+
+
+@app.post('/answer')
+def handle_answer():
+    '''Handle answer and redirect to next question page'''
+    responses.append(request.form['answer'])
+
+    if len(responses) > len(survey.questions) - 1:
+        return redirect('/completion')
+
+    return redirect(f'/questions/{len(responses)}')
+
+
+@app.get('/completion')
+def show_completion():
+    '''Show completion message at end of survey'''
+
+    print(responses)
+    questions = survey.questions
+
+    return render_template('completion.html',
+                           prompts=questions,
+                           responses=responses)
